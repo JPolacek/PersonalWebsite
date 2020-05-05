62ed8164a0fb1f06f5b0c9cacb02973f08628ba2
Jacob Polacek <jacpolacek@gmail.com>
2020-05-04 21:21:58 -0400

    I think I added a Procfile.

---
+++ docs/precache-manifest.2806888bc320ea3ccd088e184079fd55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d53fd26e0bf378871b390ad56c6bff2d",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "e8e583b77651f805a541",
+    "url": "/react-gh-pages/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "9610fa537946aeaf2947",
+    "url": "/react-gh-pages/static/js/2.045a557a.chunk.js"
+  },
+  {
+    "revision": "e8e583b77651f805a541",
+    "url": "/react-gh-pages/static/js/main.f1d072a1.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  }
+]);
